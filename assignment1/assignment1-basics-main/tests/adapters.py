49ff38d4832d1d97dbeefd6cab52dff061c4b055
Bharat Goyal <bharatgoyal5007@gmail.com>
2026-05-07 20:25:40 +0530

    bpe implemented

--- assignment1/assignment1-basics-main/tests/adapters.py
+++ assignment1/assignment1-basics-main/tests/adapters.py
@@ -8,7 +8,7 @@ import numpy.typing as npt
 import torch
 from jaxtyping import Bool, Float, Int
 from torch import Tensor
-from tokenizer_bpe import Tokenizer
+from tokenizer import Tokenizer
 
 def run_linear(
     d_in: int,
